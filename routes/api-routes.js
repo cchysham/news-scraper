c9233ffd2db36975f3338ca4dbc70f99f8268f1a
conhy <conhysham@cox.net>
2018-08-09 21:48:01 -0700

    One step forward and ten steps backwards

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,8 +5,114 @@ const cheerio = require("cheerio");
 /*db.on("error", function (error) {
   console.log("Database Error:", error);
 });*/
-
+/*
 module.exports = function (app) {
+  app.get("/articles", function (req, res) {
+
+    request("https://www.npr.org/", function (error, response, html) {
+
+      var $ = cheerio.load(html);
+
+      $("div.story-text").each(function (i, element) {
+
+        var link = $(element).children().find("a").attr("href");
+        var title = $(element).children().find("h3.title").text();
+        var summary = $(element).children().find("p.teaser").text();
+
+        db.Article.create({ title: title, link: link, summary: summary });
+
+      });
+
+      res.send("Scrape complete.");
+
+    });
+  });
+
+  app.get("/articles", function (req, res) {
+
+    db.Article.find({saved: false}, function (error, found) {
+
+      if (error) {
+        console.log(error);
+      }
+      else {
+        res.json(found);
+      }
+
+    });
+  });
+
+  app.get("/saved", function (req, res) {
+
+    db.Article.find({saved: true}, function (error, found) {
+
+      if (error) {
+        console.log(error);
+      }
+      else {
+        res.json(found);
+      }
+
+    });
+  });
+
+  app.get("/articles/:id", function(req, res) {
+
+    db.Article.findOne({ _id: req.params.id }).populate("note")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      }).catch(function(err) {
+        res.json(err);
+      });
+  });
+  
+  // NOTE ROUTE
+  /*app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body).then(function(dbNote) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true })
+      }).then(function(dbArticle) {
+        res.json(dbArticle);
+      }).catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.put("/api/articles/:id", function(req, res) {
+    db.Article.findByIdAndUpdate(req.params.id, req.body.saved),
+    (err, data) => {
+      if (err){
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    }
+  });
+
+  app.delete("/articles", function(req, res) {
+    db.Article.remove({}).then(function(err, data) {
+      res.json(data);
+    })
+  });
+
+
+
+
+
+
+}
+*/
+
+
+module.exports = function(app) {
+  var db = require("../models");
+const request = require("request");
+const cheerio = require("cheerio");
+
+/*db.on("error", function (error) {
+  console.log("Database Error:", error);
+});*/
+
+
   app.get("/scrape", function (req, res) {
 
     request("https://www.npr.org/", function (error, response, html) {
@@ -30,7 +136,7 @@ module.exports = function (app) {
 
   app.get("/articles", function (req, res) {
 
-    db.Article.find({}, function (error, found) {
+    db.Article.find({saved: false}, function (error, found) {
 
       if (error) {
         console.log(error);
@@ -41,6 +147,22 @@ module.exports = function (app) {
 
     });
   });
+
+  app.get("/saved", function (req, res) {
+
+    db.Article.find({saved: true}, function (error, found) {
+
+      if (error) {
+        console.log(error);
+      }
+      else {
+        res.json(found);
+      }
+
+    });
+  });
+
+
   app.get("/articles/:id", function(req, res) {
 
     db.Article.findOne({ _id: req.params.id }).populate("note")
@@ -62,4 +184,21 @@ module.exports = function (app) {
       });
   });
   
+  app.put("/api/articles/:id", function(req, res) {
+    db.Article.findByIdAndUpdate(req.params.id, req.body.saved),
+    (err, data) => {
+      if (err){
+        console.log(err);
+      } else {
+        res.json(data);
+      }
+    }
+  });
+
+  app.delete("/delete", function(req, res) {
+    db.Article.remove({}).then(function(err, data) {
+      res.json(data);
+    })
+  });
+
 };
